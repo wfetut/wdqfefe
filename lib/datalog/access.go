f9646445d536e89db5027219d51e4251ac1193c0
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2021-12-06 23:34:57 -0500

    [POC] Static build with musl.

--- lib/datalog/access.go
+++ lib/datalog/access.go
@@ -20,7 +20,7 @@ limitations under the License.
 package datalog
 
 // #cgo linux,386 LDFLAGS: -L${SRCDIR}/roletester/target/i686-unknown-linux-gnu/release
-// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/roletester/target/x86_64-unknown-linux-gnu/release
+// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/roletester/target/x86_64-unknown-linux-musl/release
 // #cgo linux,arm LDFLAGS: -L${SRCDIR}/roletester/target/arm-unknown-linux-gnueabihf/release
 // #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/roletester/target/aarch64-unknown-linux-gnu/release
 // #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/roletester/target/x86_64-apple-darwin/release
