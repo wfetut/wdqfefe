0cee4717338ac84270acfa9718072d37e9172a25
Tim Buckley <tim@goteleport.com>
2023-01-27 18:06:38 -0700

    Add `SessionStartV2` events, add `is_bot` flag to cert events

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -1558,6 +1558,12 @@ func (a *Server) submitCertificateIssuedEvent(req *certRequest) {
 		kubernetes = true
 	}
 
+	// Bot users are regular Teleport users, but have a special internal label.
+	bot := false
+	if _, ok := req.user.GetMetadata().Labels[types.BotLabel]; ok {
+		bot = true
+	}
+
 	// Unfortunately the only clue we have about Windows certs is the usage
 	// restriction: `RouteToWindowsDesktop` isn't actually passed along to the
 	// certRequest.
@@ -1578,6 +1584,7 @@ func (a *Server) submitCertificateIssuedEvent(req *certRequest) {
 	if err := a.AnonymizeAndSubmit(&services.UsageCertificateIssuedEvent{
 		UserName:        user,
 		Ttl:             durationpb.New(req.ttl),
+		IsBot:           bot,
 		UsageDatabase:   database,
 		UsageApp:        app,
 		UsageKubernetes: kubernetes,
