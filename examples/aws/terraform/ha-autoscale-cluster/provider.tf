d8c804608b487a36ebc1998cfbe7245692b5a146
ada <70969399+adaadb6@users.noreply.github.com>
2023-03-23 11:57:33 -0700

    Update `hashicorp/random` provider version to `3.1.1`

--- examples/aws/terraform/ha-autoscale-cluster/provider.tf
+++ examples/aws/terraform/ha-autoscale-cluster/provider.tf
@@ -11,7 +11,7 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 2.2.1"
+      version = "~> 3.1.1"
     }
     local = {
       source = "hashicorp/local"
